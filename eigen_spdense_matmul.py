1c3f8b8fd549cef523c2ee0c9d9c7c9a363de8ee
Jose Hugo Garcia (local) <ljgarcia@1605M010.icn2.net>
2023-05-24 18:19:33 +0200

    finished the dos implementation. Testing left

--- eigen_spdense_matmul.py
+++ eigen_spdense_matmul.py
@@ -1,6 +1,6 @@
 import numpy as np
 import scipy.sparse as sp
-from ctypes import CDLL, c_int, c_double, POINTER, Structure
+from ctypes import CDLL, c_int, c_double, POINTER, Structure, c_void_p,cast
 from numpy.ctypeslib import as_ctypes
 
 from numpy2ctype import copy_complex_nparray, copy_nparray, ComplexStruct, CSRStruct
@@ -10,7 +10,7 @@ from numpy2ctype import copy_complex_nparray, copy_nparray, ComplexStruct, CSRSt
 lib = CDLL("lib/eigen3_spalg.so")
 
 lib.chebyshev_density.argtypes = [ c_int, CSRStruct, CSRStruct, POINTER(ComplexStruct)]
-lib.chebyshev_density.restype = None
+lib.chebyshev_density.restype =  None
 
 
 # Create a sparse matrix and a complex vector
@@ -25,14 +25,20 @@ col_indices = copy_nparray(Ham.indices)
 row_ptr = copy_nparray(Ham.indptr)
 values = copy_complex_nparray(Ham.data)
 c_cheb_vecs = copy_complex_nparray(cheb_vecs)
-
-
 csr_tuple = CSRStruct(dim, nnz, col_indices, row_ptr, values )
 
-ntimes = c_int(1)
+#We create a structure for the number of moments
+nmom=100
+c_chebmoms = copy_complex_nparray(np.zeros(nmom, dtype=complex))
+c_nmom = c_int(nmom)
 # Perform the matrix-vector multiplication
-lib.chebyshev_density(ntimes, csr_tuple, csr_tuple, c_cheb_vecs)
+lib.chebyshev_density(nmom, csr_tuple, csr_tuple, c_cheb_vecs, c_chebmoms)
+
+# Convert the vector to a complex NumPy array
+complex_array = np.ctypeslib.as_array(values).view(np.complex128)
+
+print(complex_array)
 
 # Convert result back to numpy array
-cheb_vecs = np.array([complex(r.real, r.imag) for r in c_cheb_vecs])
-print(f"The result of the matrix-vector multiplication is: {cheb_vecs}")
+#cheb_vecs = np.array([complex(r.real, r.imag) for r in c_cheb_vecs])
+#print(f"The result of the matrix-vector multiplication is: {cheb_vecs}")
--- lib/sparse/backends/eigen3/sparse_algebra.cpp
+++ lib/sparse/backends/eigen3/sparse_algebra.cpp
@@ -8,12 +8,12 @@
 
 extern "C" 
 std::complex<double> 
-dot(std::complex<double>* x, std::complex<double>* y, int size) 
+dot(const std::complex<double>* x,const  std::complex<double>* y, int size) 
 {
     std::complex<double>
     norm = cblas_ddot(size,
-                      reinterpret_cast<double*>(x), 1,
-                      reinterpret_cast<double*>(y), 1);
+                      reinterpret_cast<const double*>(x), 1,
+                      reinterpret_cast<const double*>(y), 1);
     return norm;
 }
 
@@ -24,7 +24,7 @@ typedef Eigen::Matrix< Complex, Eigen::Dynamic, Eigen::Dynamic> EigMatrix;
 
 extern "C" 
 void 
-ctype_to_eigenspmat(ctype_util::csr x, EigSpMatrix X)
+ctype_to_eigenspmat(ctype_util::csr x, EigSpMatrix& X, bool release_memory=false)
 {
     X = EigSpMatrix(x.dim, x.dim);
     X.reserve(x.nnz);
@@ -32,15 +32,43 @@ ctype_to_eigenspmat(ctype_util::csr x, EigSpMatrix X)
         for (int j = x.row_ptr[i]; j < x.row_ptr[i+1]; ++j)
         X.insert(i, x.col_indices[j]) = Complex(x.values[j].real,x.values[j].imag);
     X.finalize();
-    //free(x.row_ptr); free(x.col_indices); free(x.values);
+    if(release_memory)
+    {
+        printf("Realing memory\n");
+        free(x.row_ptr); 
+        free(x.col_indices); 
+        free(x.values);
+    }
 };
 
+void 
+ctype_to_eigenmat(int nrows, int ncols, 
+                  ctype_util::complex* x, EigMatrix& X, 
+                  bool release_memory=false)
+{
+    // Convert the three Chebyshev vectors into a matrix
+    X = EigMatrix(nrows,ncols);
+    for (int i = 0; i < nrows; ++i)
+        for (int j = 0; j < ncols; ++j) 
+        {
+            auto idx = i * ncols + j;
+            X(i, j) = Complex(x[idx].real,x[idx].imag);
+        }
+    if(release_memory)
+    {
+        printf("Realing memory\n");
+        free(x); 
+    }
+};
+
+
 extern "C" 
-void chebyshev_density( int num_moms, 
+void  chebyshev_density( int num_moms, 
                         ctype_util::csr ham,
                         ctype_util::csr op, 
                         ctype_util::complex* cheb_vecs)
 {
+    const int dim = ham.dim;
 
     EigSpMatrix eigen_ham;
     ctype_to_eigenspmat(ham, eigen_ham);
@@ -50,24 +78,22 @@ void chebyshev_density( int num_moms,
 
     // Convert the three Chebyshev vectors into a matrix
     const int nvec = 3;
-    EigMatrix eig_cheb_vecs(ham.dim,nvec);
-    for (int i = 0; i < ham.dim; ++i)
-        for (int j = 0; j < nvec; ++j) 
-        {
-            ctype_util::complex x = cheb_vecs[j * ham.dim + i];
-            eig_cheb_vecs(i, j) = Complex(x.real,x.imag);
-        }
+    EigMatrix eig_cheb_vecs;
+    ctype_to_eigenmat(dim, nvec, cheb_vecs,eig_cheb_vecs);
 
     // Perform sparse-dense matrix multiplication
-    EigMatrix result(ham.dim,nvec);
-    for (int m=0; m < num_moms; m++)
-    {
-        Complex* Tm = eig_cheb_vecs.data();
-        //for (int n=0; n < nvec; n++)
-            //Complex mom = dot(&Tm[n*ham.dim],&Tm[n*ham.dim] , ham.dim);
+    EigMatrix result(dim,nvec);
 
-        // result = eigen_ham * eig_cheb_vecs;
-        // eig_cheb_vecs = result;
-        
+    const int eff_num_moms = int(num_moms/2)*2; 
+    auto cheb_momemts = new std::vector<Complex>(eff_num_moms);
+
+    for (int m=0; 2*m < num_moms; m++)
+    {
+        const Complex* Tm0 = eig_cheb_vecs.data()+0*dim;
+        const Complex* Tm1 = eig_cheb_vecs.data()+1*dim;
+        (*cheb_momemts)[2*m+0] = dot(Tm0,Tm0,dim);
+        (*cheb_momemts)[2*m+1] = dot(Tm0,Tm1,dim);
+        result = eigen_ham * eig_cheb_vecs;
+        eig_cheb_vecs = result;
     }
 }
